672a6008b68af9e8ababda77a858eb3959529890
Cole Willsea <coleww@gmail.com>
2014-09-22 08:34:46 -0700

    ONE MIGRATION RAILS APP

--- db/migrate/20140921223534_create_users.rb
+++ db/migrate/20140921223534_create_users.rb
@@ -3,10 +3,13 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username, null: false, limit: 255
       t.string :password_digest, null: false, limit: 255
+      t.string :session_token, null: false
       t.text :status, limit: 1024
       t.timestamps
     end
 
     add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
+
