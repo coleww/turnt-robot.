e297b1c6d16355b25a54366b782d2bb6e0357be8
Cole Willsea <coleww@gmail.com>
2014-09-27 08:34:21 -0700

    seeds!

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create username: 'bobbert', password: 'geegosh'
+User.create username: 'bobbbert', password: 'geegosh', status: '<span class="blue right border">O_O</span><span class="center red">(╯°□°）╯︵ ┻━┻)</span><span class="bold round">RADICAL!</span>'
+User.create username: 'snobbert', password: 'geegosh', status: '<span class="circular underline">=^.^=</span><span class="blue right">=^.^=</span>'
+User.create username: 'blobbert', password: 'geegosh', status: '<span class="italic">AWW YEAH</span><span class="circular underline">¯\_(ツ)_/¯</span><span class="line-through dashed-border yellow">DGAF</span>'
+User.create username: 'mamabbert', password: 'geegosh', status: '<span class="blue right border">O_O</span><span class="circular underline">O_O</span><span class="bold round">O_O</span><span class="blink overline">O_O</span><span class="blink overline">O_O</span>'
+User.create username: 'papabert', password: 'geegosh', status: '<span class="italic">YOLO!</span><span class="center red border">O_O</span><span class="bold round">AWESOME!</span><span class="line-through dashed-border yellow">O_O</span>'
+User.create username: 'qbert', password: 'geegosh', status: '<span class="center red">O_O</span><span class="blink overline border">:D</span>'
+User.create username: 'eggbert', password: 'geegosh', status: '<span class="dashed-border">=^.^=</span><span class="line-through dashed-border yellow">=^.^=</span><span class="line-through dashed-border yellow">O_O</span>'
+User.create username: 'dilbert', password: 'geegosh', status: '<span class="dashed-border">O_O</span><span class="center red border">10101000100000000</span>'
+User.create username: 'albert', password: 'geegosh', status: '<span class="blue right border">THIS IS THE NEW FACEBORK!</span>'
