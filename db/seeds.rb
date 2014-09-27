2e341df62ea7aa816a37296c75a22e6c9981cbfe
Cole Willsea <coleww@gmail.com>
2014-09-27 08:51:02 -0700

    sizing stylez

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.create username: 'bobbbert', password: 'geegosh', status: '<span class="blue right border">O_O</span><span class="center red">(╯°□°）╯︵ ┻━┻)</span><span class="bold round">RADICAL!</span>'
-User.create username: 'snobbert', password: 'geegosh', status: '<span class="circular underline">=^.^=</span><span class="blue right">=^.^=</span>'
-User.create username: 'blobbert', password: 'geegosh', status: '<span class="italic">AWW YEAH</span><span class="circular underline">¯\_(ツ)_/¯</span><span class="line-through dashed-border yellow">DGAF</span>'
-User.create username: 'mamabbert', password: 'geegosh', status: '<span class="blue right border">O_O</span><span class="circular underline">O_O</span><span class="bold round">O_O</span><span class="blink overline">O_O</span><span class="blink overline">O_O</span>'
-User.create username: 'papabert', password: 'geegosh', status: '<span class="italic">YOLO!</span><span class="center red border">O_O</span><span class="bold round">AWESOME!</span><span class="line-through dashed-border yellow">O_O</span>'
+User.create username: 'bobbbert', password: 'geegosh', status: '<span class="blue right big border">O_O</span><span class="center largest red">(╯°□°）╯︵ ┻━┻)</span><span class="bold larger round">RADICAL!</span>'
+User.create username: 'snobbert', password: 'geegosh', status: '<span class="circular underline">=^.^=</span><span class="blue large right">=^.^=</span>'
+User.create username: 'blobbert', password: 'geegosh', status: '<span class="italic">AWW YEAH</span><span class="circular large underline">¯\_(ツ)_/¯</span><span class="line-through dashed-border larger yellow">DGAF</span>'
+User.create username: 'mamabbert', password: 'geegosh', status: '<span class="blue right border">O_O</span><span class="circular large big underline">O_O</span><span class="bold round">O_O</span><span class="blink larger overline">O_O</span><span class="blink overline">O_O</span>'
+User.create username: 'papabert', password: 'geegosh', status: '<span class="largest italic">YOLO!</span><span class="center red border">O_O</span><span class="bold round">AWESOME!</span><span class="line-through dashed-border yellow">O_O</span>'
 User.create username: 'qbert', password: 'geegosh', status: '<span class="center red">O_O</span><span class="blink overline border">:D</span>'
-User.create username: 'eggbert', password: 'geegosh', status: '<span class="dashed-border">=^.^=</span><span class="line-through dashed-border yellow">=^.^=</span><span class="line-through dashed-border yellow">O_O</span>'
-User.create username: 'dilbert', password: 'geegosh', status: '<span class="dashed-border">O_O</span><span class="center red border">10101000100000000</span>'
-User.create username: 'albert', password: 'geegosh', status: '<span class="blue right border">THIS IS THE NEW FACEBORK!</span>'
+User.create username: 'eggbert', password: 'geegosh', status: '<span class="dashed-border">=^.^=</span><span class="line-through large dashed-border yellow">=^.^=</span><span class="line-through dashed-border yellow">O_O</span>'
+User.create username: 'dilbert', password: 'geegosh', status: '<span class="big dashed-border">O_O</span><span class="center red border">10101000100000000</span>'
+User.create username: 'albert', password: 'geegosh', status: '<span class="largest blue big right border">THIS IS THE NEW FACEBORK!</span>'
